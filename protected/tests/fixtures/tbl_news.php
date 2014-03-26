d9a4af23f62ac2670f0ca88c4cbeed2f86516352
cshwen <icshwen@gmail.com>
2014-03-26 09:52:46 +0800

    添加News的CRUD的Test

---
+++ protected/tests/fixtures/tbl_news.php
@@ -0,0 +1,27 @@
+<?php
+return array (
+		"news1" => array (
+				'title' => '测试一',
+				'content' => '测试一内容',
+				'author' => 'CShWen',
+				'create_time' => '1',
+				'img_name' => 'sbsbsb1',
+				'img_path' => '路径' 
+		),
+		"news2" => array (
+				'title' => '测试二',
+				'content' => '测试二内容',
+				'author' => 'CShWen',
+				'create_time' => '2',
+				'img_name' => 'sbsbsb2',
+				'img_path' => '路径2' 
+		),
+		"news3" => array (
+				'title' => '测试三',
+				'content' => '测试三内容',
+				'author' => 'CShWen',
+				'create_time' => '3',
+				'img_name' => 'sbsbsb3',
+				'img_path' => '路径3' 
+		) 
+);
--- protected/tests/unit/NewsTest.php
+++ protected/tests/unit/NewsTest.php
@@ -1,5 +1,8 @@
 <?php
 class NewsTest extends CDbTestCase {
+	public $fixtures = array (
+			'news' => 'News' 
+	);
 	public function testCRUD() {
 		$newNews = new News ();
 		$testName = 'testGeiGuiLe';
@@ -30,5 +33,45 @@ class NewsTest extends CDbTestCase {
 		$this->assertTrue ( $newNews->delete () );
 		$deletedNews = News::model ()->findByPk ( $newNewsId );
 		$this->assertEquals ( NULL, $deletedNews );
+	}
+	public function testRead() {
+		$retrievedNews = $this->news ( 'news1' );
+		$this->assertTrue ( $retrievedNews instanceof News );
+		$this->assertEquals ( '测试一', $retrievedNews->title );
+	}
+	public function testUpdate() {
+		$news = $this->news ( 'news2' );
+		$updateNewsAuthor = 'csw';
+		$news->author = $updateNewsAuthor;
+		$this->assertTrue ( $news->save ( false ) );
+		
+		$updatedNews = News::model ()->findByPk ( $news->id );
+		$this->assertTrue ( $updatedNews instanceof News );
+		$this->assertEquals ( $updateNewsAuthor, $updatedNews->author );
+	}
+	public function testDelete() {
+		$newsd = $this->news ( 'news3' );
+		$newsId = $newsd->id;
+		$this->assertTrue ( $newsd->delete () );
+		$deletedProject = News::model ()->findByPk ( $newsId );
+		$this->assertEquals ( NULL, $deletedProject );
+	}
+	public function testCreate() {
+		$newNews = new News ();
+		$newNewsContent = '测试create';
+		$newNews->setAttributes ( array (
+				'title' => '测试Create',
+				'content' => $newNewsContent,
+				'author' => 'CShWen',
+				'create_time' => '777',
+				'img_name' => 'sbsbsb7',
+				'img_path' => '路径7' 
+		) );
+		
+		$this->assertTrue ( $newNews->save ( false ) );
+		
+		$retrievedNews = News::model ()->findByPk ( $newNews->id );
+		$this->assertTrue ( $retrievedNews instanceof News );
+		$this->assertEquals ( $newNewsContent, $newNews->content );
 	}
 }
