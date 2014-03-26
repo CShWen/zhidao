c89842f05854cfc1b4952cf4094e5821358c52e9
cshwen <icshwen@gmail.com>
2014-03-26 09:15:01 +0800

    加了News的crud

---
+++ protected/views/news/_form.php
@@ -0,0 +1,70 @@
+<?php
+/* @var $this NewsController */
+/* @var $model News */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'news-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'title'); ?>
+		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'title'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'content'); ?>
+		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'content'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'author'); ?>
+		<?php echo $form->textField($model,'author',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'author'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'create_time'); ?>
+		<?php echo $form->textField($model,'create_time'); ?>
+		<?php echo $form->error($model,'create_time'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'img_name'); ?>
+		<?php echo $form->textField($model,'img_name',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'img_name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'img_path'); ?>
+		<?php echo $form->textField($model,'img_path',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'img_path'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'page_view'); ?>
+		<?php echo $form->textField($model,'page_view'); ?>
+		<?php echo $form->error($model,'page_view'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/news/_view.php
@@ -0,0 +1,43 @@
+<?php
+/* @var $this NewsController */
+/* @var $data News */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
+	<?php echo CHtml::encode($data->title); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('content')); ?>:</b>
+	<?php echo CHtml::encode($data->content); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('author')); ?>:</b>
+	<?php echo CHtml::encode($data->author); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('create_time')); ?>:</b>
+	<?php echo CHtml::encode($data->create_time); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('img_name')); ?>:</b>
+	<?php echo CHtml::encode($data->img_name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('img_path')); ?>:</b>
+	<?php echo CHtml::encode($data->img_path); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('page_view')); ?>:</b>
+	<?php echo CHtml::encode($data->page_view); ?>
+	<br />
+
+	*/ ?>
+
+</div>
